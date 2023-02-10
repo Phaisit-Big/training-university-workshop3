b1b26af7c4a37d67a0ed6ff5d685f2dabf290dfb
poramate-to <poramate.to@tnis.com>
2023-02-10 13:12:07 +0700

    quiz 3.2 bad email checker

--- complete/src/main/java/com/tn/assignment/service/validator/StudentEmailChecker.java
+++ complete/src/main/java/com/tn/assignment/service/validator/StudentEmailChecker.java
@@ -7,16 +7,11 @@ import com.tn.assignment.service.repo.StudentRepository;
 import com.tn.assignment.service.repo.entity.StudentEntity;
 
 /* 
- * TODO: Code review 3.2 - Logic performance
- *  - internal memory logic should be checked before querying databases or calling external parties
- *    e.g. the method isValid check email patterns before querying the existing of it in the database
- *  - a reuseable object could be static to help reduce unnecessary object instantiation
- *    e.g. EMAIL_PATTERN is declared once to reduce repetition of regex pattern parsing
+ * TODO: Code review 3.2 - Code performance
+ *  - modify only this class to enhance processing performance
  */
 public class StudentEmailChecker {
     
-    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
-
     private StudentRepository studentRepository;
     
 
@@ -26,17 +21,18 @@ public class StudentEmailChecker {
 
     public boolean isValid(String email) {
 
-        Matcher matcher = EMAIL_PATTERN.matcher(email);  
-        if (!matcher.matches()) {
-            // incorrect email pattern
-            return false;
-        }  
-
         StudentEntity studentEntity = studentRepository.findFirstByEmail(email);
         if (null != studentEntity) {
             // email is already used by other
             return false;
         }
+
+        Pattern pattern = Pattern.compile("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
+        Matcher matcher = pattern.matcher(email);  
+        if (!matcher.matches()) {
+            // incorrect email pattern
+            return false;
+        }  
 
         return true;
     }
